652cc1e2fd584c025d648c7100d257b19cb05471
ahmedsamyh <ahmedsamyh10@gmail.com>
2023-10-18 20:13:00 +0500

    remove typedefs

--- include/sfml-helper.hpp
+++ include/sfml-helper.hpp
@@ -15,21 +15,6 @@
 
 namespace fs = std::filesystem;
 
-namespace sh {
-#ifndef NO_TYPEDEFS
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef float f32;
-typedef double f64;
-typedef std::string str;
-#endif
-
 // macros ==================================================
 #define DEFAULT_FONT_NAME "res/font/IosevkaNerdFontMono-Regular.ttf"
 #define DEFAULT_CHAR_SIZE 32
